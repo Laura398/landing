ebdfe68c03df0c952c1f3b5b1fe24ee1fd5200db
Laura <laura@upschool.fr>
2024-05-17 12:32:15 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
                 <img src="./logo.png" alt="logo" width={80} />
               </a>
               <ul className="links">
-                <li>Portfolio</li>
+                <li><a href="https://www.portfolio.lauramathieu.com" style={{color: "white"}}>Portfolio</a></li>
                 <li><LanguageSelector /></li>
               </ul>
             </div>
@@ -26,39 +26,12 @@ function App() {
                 <img className="photo" src="/assets/photo.png" alt="photo" />
                 <h1>Laura Mathieu</h1>
                 <h2>{t('dev')}</h2>
-                <button>{t('discover')}</button>
+                <a href="https://www.portfolio.lauramathieu.com"><button>{t('discover')}</button></a>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      {/* <div style={{ position: "absolute", top: "10px", right: "10px" }}>
-        <LanguageSelector />
-      </div>
-      <div className="App">
-        <header className="App-header">
-          <Avatar
-            src="/assets/photo.png"
-            sx={{
-              "--Avatar-size": "8rem",
-              border: "1px solid white",
-              margin: "auto",
-            }}
-          />
-          <Typography level="h1" mt={5}>
-            Laura Mathieu
-          </Typography>
-          <Typography level="h3" mb={5}>
-            {t("dev")}
-          </Typography>
-          <a href="https://www.portfolio.lauramathieu.com">
-            <Button color="neutral" sx={{ m: "5" }}>
-              {t("discover")}
-            </Button>
-          </a>
-        </header>
-      </div> */}
     </>
   );
 }
